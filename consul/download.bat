4ba02b378c67c2f85a2fa378fcaba6bc61700860
Calvin Leung Huang <cleung2010@gmail.com>
2016-01-22 13:53:36 -0500

    Use env var on download.bat

--- consul/download.bat
+++ consul/download.bat
@@ -1,8 +1,9 @@
 if exist binaries rmdir /s /q binaries
+SET CONSUL_VERSION=0.6.3
 mkdir binaries
 pushd binaries
-curl -k -fsSL https://releases.hashicorp.com/consul/0.6.3/consul_0.6.3_windows_amd64.zip > 0.6.3_windows_386.zip
-curl -k -fsSL https://releases.hashicorp.com/consul/0.6.3/consul_0.6.3_web_ui.zip > 0.6.3_web_ui.zip
-curl -k -fsSL https://releases.hashicorp.com/consul/0.6.3/consul_0.6.3_SHA256SUMS > 0.6.3_SHA256SUMS
+curl -k -fsSL https://releases.hashicorp.com/consul/%CONSUL_VERSION%/consul_%CONSUL_VERSION%_windows_amd64.zip > %CONSUL_VERSION%_windows_386.zip
+curl -k -fsSL https://releases.hashicorp.com/consul/%CONSUL_VERSION%/consul_%CONSUL_VERSION%_web_ui.zip > %CONSUL_VERSION%_web_ui.zip
+curl -k -fsSL https://releases.hashicorp.com/consul/%CONSUL_VERSION%/consul_%CONSUL_VERSION%_SHA256SUMS > %CONSUL_VERSION%_SHA256SUMS
 curl -k -fsSL https://nssm.cc/release/nssm-2.24.zip > nssm-2.24.zip
 popd
