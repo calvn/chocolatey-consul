ed1a33d98de35eb8fbd3c7e6496f7c32390d53dc
Calvin Leung Huang <cleung2010@gmail.com>
2016-01-22 14:56:02 -0500

    Use nssm package dependency instead of having it in source

--- consul/download.bat
+++ consul/download.bat
@@ -5,5 +5,4 @@ pushd binaries
 curl -k -fsSL https://releases.hashicorp.com/consul/%CONSUL_VERSION%/consul_%CONSUL_VERSION%_windows_amd64.zip > %CONSUL_VERSION%_windows_amd64.zip
 curl -k -fsSL https://releases.hashicorp.com/consul/%CONSUL_VERSION%/consul_%CONSUL_VERSION%_web_ui.zip > %CONSUL_VERSION%_web_ui.zip
 curl -k -fsSL https://releases.hashicorp.com/consul/%CONSUL_VERSION%/consul_%CONSUL_VERSION%_SHA256SUMS > %CONSUL_VERSION%_SHA256SUMS
-curl -k -fsSL https://nssm.cc/release/nssm-2.24.zip > nssm-2.24.zip
 popd
