8bea396cfab8953501ebb23ff1506c5e56babf16
Luk Vermeulen <luk.vermeulen@gmail.com>
2016-09-19 06:28:40 +0200

    Update to Consul 0.7.0 (#9)

--- consul/download.bat
+++ consul/download.bat
@@ -1,5 +1,5 @@
 if exist binaries rmdir /s /q binaries
-SET CONSUL_VERSION=0.6.4
+SET CONSUL_VERSION=0.7.0
 mkdir binaries
 pushd binaries
 curl -k -fsSL https://releases.hashicorp.com/consul/%CONSUL_VERSION%/consul_%CONSUL_VERSION%_windows_amd64.zip > %CONSUL_VERSION%_windows_amd64.zip
