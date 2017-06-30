7800e8eddc7f5e789fbfb294b55195bf0b740de7
Calvin Leung Huang <cleung2010@gmail.com>
2017-06-29 22:02:36 -0400

    Update to version 0.8.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -16,7 +16,7 @@ if (-not ($packageParameters)) {
 }
 
 # Consul related variables
-$consulVersion = '0.8.1'
+$consulVersion = '0.8.2'
 $sourcePath = if (Get-ProcessorBits 32) {
   $(Join-Path $binariesPath "$($consulVersion)_windows_386.zip")
 } else {
