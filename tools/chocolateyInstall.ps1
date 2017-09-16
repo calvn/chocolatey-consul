15c75a3a255e17dd7402e3055c9c629208e617d8
Calvin Leung Huang <cleung2010@gmail.com>
2017-09-16 15:45:12 -0400

    Update to version 0.9.3 (#12)

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -15,7 +15,7 @@ if (-not ($packageParameters)) {
 }
 
 # Consul related variables
-$consulVersion = '0.9.2'
+$consulVersion = '0.9.3'
 $sourcePath = if (Get-ProcessorBits 32) {
   $(Join-Path $binariesPath "$($consulVersion)_windows_386.zip")
 } else {
