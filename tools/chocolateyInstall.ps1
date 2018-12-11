6e22e35b7cdfb8cc485c37f49bd55a36dc9cff70
Shawn Toffel <shawn.toffel+github@gmail.com>
2018-12-11 12:15:45 -0700

    Update Consul to v1.4.0 (#28)

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -15,7 +15,7 @@ if (-not ($packageParameters)) {
 }
 
 # Consul related variables
-$consulVersion = '1.3.1'
+$consulVersion = '1.4.0'
 $sourcePath = if (Get-ProcessorBits 32) {
   $(Join-Path $binariesPath "$($consulVersion)_windows_386.zip")
 } else {
