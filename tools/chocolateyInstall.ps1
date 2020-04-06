543089f6b35cb8792f16f7f2d0eb603bb3d0af0c
mahsoud <mahsoud@users.noreply.github.com>
2020-04-06 21:33:57 +0300

    Update chocolateyInstall.ps1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -15,7 +15,7 @@ if (-not ($packageParameters)) {
 }
 
 # Consul related variables
-$consulVersion = '1.6.2'
+$consulVersion = '1.7.2'
 
 $sourcePath = if (Get-ProcessorBits 32) {
   $(Join-Path $binariesPath "$($consulVersion)_windows_386.zip")
