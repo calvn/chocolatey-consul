e4250a36650a763b4fc35140bb08414b03484c44
Kristjanf-droid <67269523+Kristjanf-droid@users.noreply.github.com>
2020-10-29 13:50:30 +0000

    Updating Consul to 1.8.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -15,7 +15,7 @@ if (-not ($packageParameters)) {
 }
 
 # Consul related variables
-$consulVersion = '1.8.0'
+$consulVersion = '1.8.1'
 $sourcePath = if (Get-ProcessorBits 32) {
   $(Join-Path $binariesPath "$($consulVersion)_windows_386.zip")
 } else {
