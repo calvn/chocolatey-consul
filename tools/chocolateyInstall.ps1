3397f7c27e048f4a9f2cca262f3e02353274c617
Calvin Leung Huang <cleung2010@gmail.com>
2020-12-27 17:09:02 -0800

    Update Consul to version 1.8.6 (#68)

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -15,7 +15,7 @@ if (-not ($packageParameters)) {
 }
 
 # Consul related variables
-$consulVersion = '1.8.5'
+$consulVersion = '1.8.6'
 $sourcePath = if (Get-ProcessorBits 32) {
   $(Join-Path $binariesPath "$($consulVersion)_windows_386.zip")
 } else {
