a28225ec97d739341f214b9ed2291c4e35eb83f5
Jason Starman <jstarman@incomm.com>
2021-09-07 09:29:46 -0700

    Update to version 1.10.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -15,7 +15,7 @@ if (-not ($packageParameters)) {
 }
 
 # Consul related variables
-$consulVersion = '1.9.0'
+$consulVersion = '1.10.2'
 $sourcePath = if (Get-ProcessorBits 32) {
   $(Join-Path $binariesPath "$($consulVersion)_windows_386.zip")
 } else {
